6c4d93ddb7ea290c14c6ea0923d79d2676d1450d
kurnianto43 <yanuarkurnianto1@gmail.com>
2018-01-05 17:48:56 +0800

    Membuat method koneksi pada class ConfigDb

--- src/rentalmobil/ConfigDb.java
+++ src/rentalmobil/ConfigDb.java
@@ -5,10 +5,39 @@
  */
 package rentalmobil;
 
+import java.sql.*;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JOptionPane;
+import java.io.File;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JRDesignQuery;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 /**
  *
  * @author asus
  */
 public class ConfigDb {
+    private String Password="";
+    private String Username="root";
+    private String Database="db_rentalan";
     
+    public Connection koneksi;
+
+    public ConfigDb(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            koneksi=DriverManager.getConnection("jdbc.mysql://localhost:3306/"+this.Database,
+                    this.Username,this.Password);
+                    System.out.println("Koneksi Berhasil");
+                    
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Maaf terjadi kesalahan pada bagian : \n["+e.toString()+"]");
+        }
+    }
 }
